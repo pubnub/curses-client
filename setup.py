9fe633b42ccdf0d2e495905e110f79df857b8865
dristic <dristic101@gmail.com>
2014-02-10 15:45:05 -0800

    Setup for pip installing.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='PubNub Curses',
+  version='1.0',
+  description='PubNub Curses Terminal Client',
+  author='Dan Ristic',
+  author_email='danr@pubnub.com',
+  url='http://pubnub.com',
+  packages=['pubnub']
+)
